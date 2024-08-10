677c0ff0d20b0807a914df074e3d7816a49a00a0
SurVile <145489772+SurVile@users.noreply.github.com>
2024-08-10 21:03:08 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,5 +3,16 @@ let tg = window.Telegram.WebApp;
 let send_reg = document.querySelector('[name="send_reg"]');
 
 send_reg.addEventListener('click', () => {
+    let user_name = document.querySelector('[name="user_full_name"]'),
+        user_email = document.querySelector('[name="user_email"]'),
+        user_tel = document.querySelector('[name="user_tel"]');
+
+    let user_data = {
+        data_name: user_name,
+        data_email: user_email,
+        data_tel: user_tel
+    }
+    tg.sendData(JSON.stringify(user_data));
+
     tg.close();
 })
