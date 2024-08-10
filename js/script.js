45db07c0b19f1aa6495c8f0203d5b607cd0e7a96
SurVile <145489772+SurVile@users.noreply.github.com>
2024-08-11 00:01:13 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let tg = window.Telegram.WebApp;
 
-let send_reg = document.querySelector('[name="send_reg"]');
+let send_reg = document.querySelector('[name="send_reg"]'),
+    buy_item = document.querySelectorAll('[name="buy_item"]');
 
 send_reg.addEventListener('click', () => {
     let user_name = document.querySelector('[name="user_full_name"]').value,
@@ -10,6 +11,7 @@ send_reg.addEventListener('click', () => {
     console.log(user_name + ', ' + user_email + ', ' + user_tel)
 
     let user_data = {
+        data_type: 'user_reg',
         data_name: user_name,
         data_email: user_email,
         data_tel: user_tel
@@ -18,3 +20,16 @@ send_reg.addEventListener('click', () => {
 
     tg.close();
 })
+
+buy_item.addEventListener('click', () => {
+    let button_block_info = this.parentElement;
+    let article_name = button_block_info.parentElement.name;
+
+    let article_data = {
+        data_type: 'buy_item',
+        data_name: article_name
+    }
+    tg.sendData(JSON.stringify(article_data));
+
+    tg.close();
+});
